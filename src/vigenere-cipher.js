c096333411fc1420c36c3eeb0acc01cfdad0bd4a
VasaSkor <dzho.ker.700@gmail.com>
2022-10-16 22:21:38 +0300

    feat: add complete task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,89 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isReverse = true){
+    this.isReverse = !isReverse
+    this.alphabet  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if(!message || !key){
+      throw new Error('Incorrect arguments!')
+    }
+      let result = []
+      const msg = message.toUpperCase().split('')
+      const k = key.toUpperCase().split('')
+      let count = 0
+      for (let i = 0; i < msg.length; i++) {
+        if(!this.alphabet.includes(msg[i])){
+          count++
+          result.push(msg[i])
+          continue 
+        }
+        const msgIndex = this.alphabet.indexOf(msg[i])
+        const keyIndex = this.alphabet.indexOf(getArrElem(k, Math.abs(i - count)))
+        result.push(this.calcEncryptLetter(msgIndex, keyIndex))
+      }
+      if (this.isReverse) {
+        return result.reverse().join('')
+      }
+      return result.join('')
   }
+
+  calcEncryptLetter(mj, kj){
+    return getArrElem(this.alphabet , mj + kj % this.alphabet.length)
+  }
+
+  calcDecryptLetter(mj, kj){
+    return getArrElem(this.alphabet , mj - kj % this.alphabet.length)
+  }
+
+  decrypt(encryptedMessage , key) {
+    if(!encryptedMessage || !key){
+      throw new Error('Incorrect arguments!')
+    }
+      let result = []
+      const msg = encryptedMessage.toUpperCase().split('')
+      const k = key.toUpperCase().split('')
+      let count = 0
+      for (let i = 0; i < msg.length; i++) {
+        if(!this.alphabet.includes(msg[i])){
+          count++
+          result.push(msg[i])
+          continue 
+        }
+        const msgIndex = this.alphabet.indexOf(msg[i])
+        const keyIndex = this.alphabet.indexOf(getArrElem(k, Math.abs(i - count)))
+        result.push(this.calcDecryptLetter(msgIndex, keyIndex))
+      }
+      if (this.isReverse) {
+        return result.reverse().join('')
+      }
+      
+      return result.join('')
+  }
+}
+
+function getArrElem(arr, i) {
+  const _getArrElem = (arr, i) =>{
+    if(i > 0){
+      if(arr.at(i) !== undefined) {
+        return arr.at(i)
+      }
+      else{
+        return _getArrElem(arr, i - arr.length)
+      }
+    }
+    else{
+      if(arr.at(i) !== undefined) {
+        return arr.at(i)
+      }
+      else{
+        return _getArrElem(arr, arr.length - i - 1)
+      }
+    }
+  }
+  return _getArrElem(arr, i)
 }
 
 module.exports = {
