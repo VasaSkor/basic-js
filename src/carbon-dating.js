19d3cd02e67677de3cf458fbf6485feab355640b
VasaSkor <dzho.ker.700@gmail.com>
2022-10-16 17:53:40 +0300

    feat: add complete task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(typeof(sampleActivity) !== 'string'){
+    return false
+  }
+
+  if(Number.isNaN(+sampleActivity) || +sampleActivity === 0 ){
+    return false
+  }
+
+  const res = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD))
+  if(res > 0 ){
+    return res
+  }
+  else{
+    return false
+  }
 }
 
 module.exports = {
