4d570cfadc2fa60c9452797ef32c8ebe059f5210
VasaSkor <dzho.ker.700@gmail.com>
2022-10-18 02:39:54 +0300

    feat: add complete task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,8 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(str) {
-  
+ function encodeLine(str) {
+  let s = ''
+  const arr = str.split('')
+  const res = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] !== arr[i]) {
+      s += arr[i]
+      if (s === '') {
+        res.push('')
+      } else {
+        res.push(s)
+      }
+      s = ''
+      continue
+    }
+
+    s += arr[i]
+  }
+
+  return res.map(e => `${e.length > 1 ? e.length : ''}${e.split('')[0]}`).join('')
 }
 
 module.exports = {
